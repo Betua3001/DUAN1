38b9602434c5fb6d69c3e74435f10f1336752e05
VoThanhTai19 <93961593+VoThanhTai19@users.noreply.github.com>
2021-12-03 19:51:48 +0700

    Đăng nhập bằng google

--- app/src/main/java/com/example/sellapp/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/sellapp/activities/WelcomeActivity.java
@@ -1,27 +1,43 @@
 package com.example.sellapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.sellapp.MainActivity;
 import com.example.sellapp.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class WelcomeActivity extends AppCompatActivity {
     ProgressBar progressBar;
     FirebaseUser user;
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth auth;
+    private static final int RC_SIGN_IN = 2001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         user = FirebaseAuth.getInstance().getCurrentUser();
+        auth =  FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressbar);
         progressBar.setVisibility(View.GONE);
         if (user != null) {
@@ -30,6 +46,15 @@ public class WelcomeActivity extends AppCompatActivity {
             Toast.makeText(this, "Please wait you are already logged in", Toast.LENGTH_SHORT).show();
             finish();
         }
+
+        // Định cấu hình Đăng nhập Google
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
     }
 
     public void login(View view) {
@@ -39,4 +64,46 @@ public class WelcomeActivity extends AppCompatActivity {
     public void registration(View view) {
         startActivity(new Intent(WelcomeActivity.this, RegistrationActivity.class));
     }
+
+    public void googleSignIn(View view) {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Toast.makeText(WelcomeActivity.this, "Lỗi: "+e, Toast.LENGTH_SHORT).show();
+                Log.d("loi", ""+e);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(WelcomeActivity.this, "Loggin Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(WelcomeActivity.this, "Loggin Failed", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
 }
